9d8d9bf0bb05494341128db6202f2689a2cf5d6d
kibitan <uzukifirst@gmail.com>
2017-12-31 00:01:57 +0100

    gradually implementation: implement /v1/swagger, define format

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    resource :swagger, only: :show
+    resource :swagger, only: :show, constraints: ->(req) { req.format == :json }
   end
 end
--- spec/requests/v1/swaggers_spec.rb
+++ spec/requests/v1/swaggers_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "V1::Swagger", type: :request do
   describe "GET /v1/swagger" do
-    it "works! (now write some real specs)" do
-      get v1_swagger_path
+    it "no routes error" do
+      expect { get v1_swagger_path }.to raise_error ActionController::RoutingError
+    end
+  end
+
+  describe "GET /v1/swagger.json" do
+    it "return swagger.json" do
+      get v1_swagger_path(format: :json)
+
       expect(response).to have_http_status(200)
     end
   end
