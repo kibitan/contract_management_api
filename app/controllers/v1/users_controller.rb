f91e2c3d837033a28b72dc6176b3c47badd18db6
kibitan <uzukifirst@gmail.com>
2018-01-04 16:26:19 +0100

    gradually implementation: implement /v1/users, Fake it!

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,51 +1,11 @@
 class V1::UsersController < ApplicationController
-  before_action :set_v1_user, only: [:show, :update, :destroy]
-
-  # GET /v1/users
-  def index
-    @v1_users = V1::User.all
-
-    render json: @v1_users
-  end
-
-  # GET /v1/users/1
-  def show
-    render json: @v1_user
-  end
-
   # POST /v1/users
   def create
-    @v1_user = V1::User.new(v1_user_params)
-
-    if @v1_user.save
-      render json: @v1_user, status: :created, location: @v1_user
-    else
-      render json: @v1_user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /v1/users/1
-  def update
-    if @v1_user.update(v1_user_params)
-      render json: @v1_user
-    else
-      render json: @v1_user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /v1/users/1
-  def destroy
-    @v1_user.destroy
+    render status: :created, json: {auth_token: 'token12345' }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_v1_user
-      @v1_user = V1::User.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def v1_user_params
-      params.fetch(:v1_user, {})
-    end
+  def user_params
+    params.fetch(:v1_user, {})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
     resource :swagger, only: :show, constraints: ->(req) { req.format == :json }
+    resources :users, only: :create
   end
 end
