7a309a970bdf116c438cf4ce33311a0a48da5a8c
kibitan <uzukifirst@gmail.com>
2018-01-04 16:12:55 +0100

    gradually implementation: imoplement test for /v1/users

--- spec/requests/v1/api_spec.rb
+++ spec/requests/v1/api_spec.rb
@@ -2,6 +2,35 @@ require 'rails_helper'
 
 describe 'API', type: :apivore, order: :defined do
   subject { Apivore::SwaggerChecker.instance_for('/v1/swagger.json') }
+  # @see https://qiita.com/kymmt90/items/26c493e5f751a7e4f946
+  let(:path_params) { {} }
+  let(:headers) { {} }
+  let(:query_params) { {} }
+  let(:data_params) { {} }
+  let(:params) {
+    path_params.merge(
+      '_headers' => headers,
+      '_query_string' => query_params.to_query,
+      '_data' => data_params
+    )
+  }
+
+  context 'has valid paths', order: :random do
+    context '/v1/users' do
+      let(:full_name) { Faker::Name.name }
+      let(:email) { Faker::Internet.email }
+      let(:password) { Faker::Internet.password }
+      let(:data_params) do
+        {
+          full_name: full_name,
+          email: email,
+          password: password
+        }
+      end
+
+      it { is_expected.to validate(:post, '/users', 201, params) }
+    end
+  end
 
   context 'and' do
     it 'tests all documented routes' do
